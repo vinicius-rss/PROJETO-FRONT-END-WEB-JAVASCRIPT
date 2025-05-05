cbc32cf43418a1a59e864b37fe13b21b4af9a630
Vinicius Rodrigues <vinicius.santos.job@gmail.com>
2025-05-04 22:03:11 -0300

    enviando código local para repo

---
+++ retro movie app/retro-movie-app/script.js
@@ -0,0 +1,26 @@
+const filmesContainer = document.getElementById("filmes-container");
+const apiKey = ""; 
+const url = `https://api.themoviedb.org/3/movie/popular?language=pt-BR&api_key=${apiKey}`;
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    const filmes = data.results.slice(0, 10);
+    filmes.forEach(filme => {
+      const card = document.createElement("div");
+      card.className = "filme";
+
+      const posterUrl = `https://image.tmdb.org/t/p/w300${filme.poster_path}`;
+
+      card.innerHTML = `
+        <img src="${posterUrl}" alt="Capa de ${filme.title}">
+        <h2>${filme.title}</h2>
+        <p><strong>Nota:</strong> ${filme.vote_average}</p>
+        <p><strong>Idioma:</strong> ${filme.original_language.toUpperCase()}</p>
+        <p><strong>Popularidade:</strong> ${filme.popularity}</p>
+      `;
+
+      filmesContainer.appendChild(card);
+    });
+  })
+  .catch(err => console.error("Erro ao buscar os filmes:", err));
